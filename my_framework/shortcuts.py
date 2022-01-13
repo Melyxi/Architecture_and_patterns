017bc87225d11803d7661df07d481f42c476687a
igor <igor@localhost.localdomain>
2022-01-13 15:44:07 +0300

    lesson3

--- my_framework/shortcuts.py
+++ my_framework/shortcuts.py
@@ -1,4 +1,4 @@
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 from os.path import join
 
 
@@ -6,8 +6,12 @@ def render(request, template_name, folder='templates', context=None, **kwargs):
     if context is None:
         context = {}
     context = {**{'request': request}, **context}
-    file_path = join(folder, template_name)
-    with open(file_path, encoding='utf-8') as file:
-        template = Template(file.read())
+
+    # создаем объект окружения
+    env = Environment()
+    # указываем папку для поиска шаблонов
+    env.loader = FileSystemLoader(folder)
+    # находим шаблон в окружении
+    template = env.get_template(template_name)
 
     return template.render(context)
