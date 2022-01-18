5082870c722389bd6f99f6d62633b72f6c5cacd4
igor <igor@localhost.localdomain>
2022-01-18 11:31:35 +0300

    fix

--- middlewares.py
+++ middlewares.py
@@ -8,4 +8,9 @@ def secret_front(request):
 def other_front(request):
     request['key'] = 'key'
 
-fronts = [secret_front, other_front]
+
+def fake_request(request):
+    print('200 OK', 'Hello from Fake')
+
+
+fronts = [secret_front, other_front, fake_request]
